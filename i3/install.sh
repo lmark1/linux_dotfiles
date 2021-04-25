fb807b5c49e42f163fd463184e2f6672d7658fdb
Lovro Markovic <lovro.markovic@fer.hr>
2021-04-25 22:33:33 +0200

    [i3] Add set brightness

--- i3/install.sh
+++ i3/install.sh
@@ -10,3 +10,20 @@ APP_PATH=`dirname "$0"`
 APP_PATH=`( cd "$APP_PATH" && pwd )`
 
 sudo apt install -y i3 i3blocks
+
+# required for i3-layout-manager
+sudo apt -y install jq rofi xdotool x11-xserver-utils indent libanyevent-i3-perl
+
+sudo apt-get -y install lightdm
+
+# for cpu usage in i3blocks
+sudo apt-get -y install sysstat
+
+# for brightness and volume control
+sudo apt-get -y install xbacklight alsa-utils pulseaudio feh arandr
+
+# for making gtk look better
+sudo apt-get -y install lxappearance
+
+# install useful gui utils
+sudo apt-get -y install thunar rofi compton systemd
--- i3/link.sh
+++ i3/link.sh
@@ -11,3 +11,8 @@ APP_PATH=`( cd "$APP_PATH" && pwd )`
 
 ln -fs $APP_PATH/doti3config ~/.config/i3/config
 ln -fs $APP_PATH/doti3blocks ~/.config/i3/i3blocks.conf
+
+# to run the setBrightness.sh
+# add following lines to /etc/sudoers below the line: %sudo	ALL=(ALL:ALL) ALL
+# %<USER_NAME> ALL=(root) NOPASSWD: /home/<USER_NAME>/.config/i3/setBrightness.sh
+ln -fs $APP_PATH/setBrightness.sh ~/.config/i3/setBrightness.sh
