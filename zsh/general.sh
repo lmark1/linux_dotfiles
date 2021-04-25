1ed6d23ec4e006f283608a85679468303d6aaeb6
Lovro Markovic <lovro.markovic@fer.hr>
2021-04-25 18:02:15 +0200

    Update general functions.

--- zsh/general.sh
+++ zsh/general.sh
@@ -17,3 +17,78 @@ killp() {
 # echo "killing $1"
 kill -9 "$1" > /dev/null 2> /dev/null
 }
+
+# running new tmux (or attaching) with session name derived from parent zsh pid
+runTmux() {
+
+  SESSION_NAME="T$PPID"
+
+  # try to find session with the correct session id (based on the zsh PID)
+  EXISTING_SESSION=`$TMUX_BIN ls 2> /dev/null | grep "$SESSION_NAME" | wc -l`
+
+  if [ "$EXISTING_SESSION" -gt "0" ]; then
+
+    # if such session exists, attach to it
+    $TMUX_BIN -2 attach-session -t "$SESSION_NAME"
+
+  else
+
+    # if such session does not exist, create it
+    $TMUX_BIN new-session -s "$SESSION_NAME"
+
+  fi
+
+  # hook after exitting the session
+  # when the session exists, find a file in /tmp with the name of the session
+  # and extract a path from it. Then cd to it.
+  FILENAME="/tmp/tmux_restore_path.txt"
+  if [ -f $FILENAME ]; then
+
+    MY_PATH=$(tail -n 1 $FILENAME)
+
+    rm /tmp/tmux_restore_path.txt
+
+    cd $MY_PATH
+
+  fi
+}
+
+
+zshexit() {
+
+  pwd >> /tmp/tmux_restore_path.txt
+
+  forceKillTmuxSession "T$PPID"
+}
+
+# VIM alias for running vim in tmux and in servermode
+runVim() {
+
+  VIM_CMD=$(echo "$EDITOR ${@}")
+
+  # if the tmux session does not exist, create new and run vim in it
+  if [ -z $TMUX ]; then
+
+    SESSION_NAME="T$PPID"
+
+    # if there is a tmux session with the same name as the current bashpid
+    num=`$TMUX_BIN ls 2> /dev/null | grep "$SESSION_NAME" | wc -l`
+    if [ "$num" -gt "0" ]; then
+
+      ID=`$TMUX_BIN new-window -t "$SESSION_NAME" -a -P`
+      sleep 1.0
+      $TMUX_BIN send-keys -t $ID "$VIM_CMD" C-m
+      $TMUX_BIN -2 attach-session -t "$SESSION_NAME"
+
+    else
+
+      $TMUX_BIN new-session -s "$SESSION_NAME" -d "$VIM_CMD" \; attach
+
+    fi
+
+  else
+
+    zsh -c "$VIM_CMD"
+
+  fi
+}
